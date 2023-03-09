583937f03dcf642420883bc4730e2740ab389338
CBTahir <127340931+CBTahir@users.noreply.github.com>
2023-03-09 19:19:41 +0100

    Add files via upload

---
+++ DownHalfPyramid.py
@@ -0,0 +1,16 @@
+def Ej(a,b):
+    c=0
+    for i in range(0,b):
+        for j in range (0,b):
+            print(a, end=" ")
+        b = b-1
+        print("\n")
+
+
+
+
+
+
+
+
+print(Ej("l", 5))
